b0534e9f8c1650b00d7497af46baa93209522591
Sarthak Kumar Rout <routsarthakkumar170@gmail.com>
2026-08-01 11:02:37 +0530

    Added Lab 01 Question 2 - Fair vs Biased Coin Simulation

---
+++ Lab-01/prog2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    int tosses;
+    int fairHead = 0, fairTail = 0;
+    int biasedHead = 0, biasedTail = 0;
+
+    printf("Enter number of coin tosses: ");
+    scanf("%d", &tosses);
+
+    srand(time(NULL));
+
+    // Fair Coin Simulation
+    for(int i = 0; i < tosses; i++)
+    {
+        if(rand() % 2)
+            fairHead++;
+        else
+            fairTail++;
+    }
+
+    // Biased Coin Simulation (70% Head)
+    for(int i = 0; i < tosses; i++)
+    {
+        int r = rand() % 100;
+
+        if(r < 70)
+            biasedHead++;
+        else
+            biasedTail++;
+    }
+
+    printf("\n========== FAIR COIN ==========\n");
+    printf("Heads = %d\n", fairHead);
+    printf("Tails = %d\n", fairTail);
+    printf("Probability of Head = %.4f\n",
+           (float)fairHead / tosses);
+
+    printf("\n========== BIASED COIN (70%% Head) ==========\n");
+    printf("Heads = %d\n", biasedHead);
+    printf("Tails = %d\n", biasedTail);
+    printf("Probability of Head = %.4f\n",
+           (float)biasedHead / tosses);
+
+    return 0;
+}
