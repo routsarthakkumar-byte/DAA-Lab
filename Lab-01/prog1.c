d0e45d22ab4bb2f5c1e0ba7b0a0247b2203920a8
Sarthak Kumar Rout <routsarthakkumar170@gmail.com>
2026-08-01 10:51:16 +0530

    Initial commit: Added DAA Lab 01 Question 1

---
+++ Lab-01/prog1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Increasing Order of Growth:\n\n");
+
+    printf("1. 1/n\n");
+    printf("2. 12*sqrt(n)\n");
+    printf("3. 50*n^0.5\n");
+    printf("4. n^0.51\n");
+    printf("5. n*log2(n)\n");
+    printf("6. n^2 - 324\n");
+    printf("7. 100*n^2 + 6*n\n");
+    printf("8. 2*n^3\n");
+    printf("9. n^(log2(n))\n");
+    printf("10. 2^(32*n) * log2(n)\n");
+    printf("11. 3^n\n");
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Design and Analysis of Algorithms (DAA) Lab
+
+This repository contains all the laboratory assignments for the Design and Analysis of Algorithms (DAA) course.
+
+## Student Details
+
+- **Name:** Sarthak Kumar Rout
+- **Roll-No:** B425048
+- **Branch:** CSE-B
+- **Semester:** 3rd Semester
+- **Course:** B.Tech
+- **Subject:** Design and Analysis of Algorithms
+- **Institute:** IIIT Bhubaneswar
+
+---
