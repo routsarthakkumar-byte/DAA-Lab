39ae4f25a8cf38052336d868eab98a60454ecfec
Sarthak Kumar Rout <routsarthakkumar170@gmail.com>
2026-08-01 11:13:46 +0530

    Added Lab 01 Question 4 - Tower of Hanoi

---
+++ Lab-01/prog4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int moves = 0;
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination)
+{
+    if(n == 1)
+    {
+        printf("Move Disk 1 from %c to %c\n", source, destination);
+        moves++;
+        return;
+    }
+
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+
+    printf("Move Disk %d from %c to %c\n", n, source, destination);
+    moves++;
+
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+
+    towerOfHanoi(n, 'A', 'B', 'C');
+
+    printf("\nTotal Moves = %d\n", moves);
+
+    return 0;
+}
